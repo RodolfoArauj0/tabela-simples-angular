95ec6304be3c73195203900f574192a1eda2873d
RodolfoArauj0 <arujo.ro@hotmail.com>
2022-08-12 19:35:27 -0300

    Add lista de cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -8,39 +9,11 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit {
     courses: Course[] = [];
 
+    constructor(private courseService:  CourseService) { }
+
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: './assets/images/forms.png',
-                price: 99.09,
-                code: 'XPS-8973',
-                duration: 120,
-                rating: 4.4,
-                releaseDate: 'Dec de 2010'
-            },
-            {
-                id: 2,
-                name: 'Matemática avançada',
-                imageUrl: './assets/images/http.png',
-                price: 79.09,
-                code: 'XPS-3373',
-                duration: 90,
-                rating: 3.5,
-                releaseDate: 'Jan de 2012'
-            },
-            {
-                id: 3,
-                name: 'Basico ao avançado CLI Angular',
-                imageUrl: './assets/images/cli.png',
-                price: 79.09,
-                code: 'XOS-8932',
-                duration: 80.80,
-                rating: 4,
-                releaseDate: 'Jan de 2012'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
 
 }
--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnChanges } from "@angular/core";
 export class StarComponent implements OnChanges {
     @Input()
     rating: number = 0;
-    starWidth: number;
+    starWidth: number = 0;
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 74 / 5
